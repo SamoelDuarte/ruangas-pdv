f4e640fdd69b6c5e000a16dd3a7c1676d4a52f2a
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-05-18 17:23:57 -0300

    Update GitWebhookController.php

--- app/Http/Controllers/GitWebhookController.php
+++ app/Http/Controllers/GitWebhookController.php
@@ -8,22 +8,12 @@ class GitWebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        // Opcional: validar segredo do webhook para segurança
-        // $secret = env('GIT_WEBHOOK_SECRET');
-        // $signature = 'sha1=' . hash_hmac('sha1', $request->getContent(), $secret);
-        // if (!hash_equals($signature, $request->header('X-Hub-Signature'))) {
-        //     return response('Unauthorized', 401);
-        // }
-
-        // Executar o git pull
         $output = null;
         $returnVar = null;
         
-        // Caminho da pasta do seu projeto
-        $projectPath = base_path(); // normalmente a raiz do Laravel
+        $projectPath = base_path();
 
-        // Comando para atualizar o repositório
-        exec("cd {$projectPath} && git pull origin main 2>&1", $output, $returnVar);
+        exec("cd {$projectPath} && git reset --hard && git clean -fd && git pull origin main 2>&1", $output, $returnVar);
 
         if ($returnVar !== 0) {
             return response()->json([

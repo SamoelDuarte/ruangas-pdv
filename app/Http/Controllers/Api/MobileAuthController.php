0822eb3113c9bf913331ae366c53f66fea4fcd73
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-05-18 20:07:52 -0300

    Update MobileAuthController.php

--- app/Http/Controllers/Api/MobileAuthController.php
+++ app/Http/Controllers/Api/MobileAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Entregador;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
@@ -11,9 +12,9 @@ class MobileAuthController extends Controller
 {
     public function login(Request $request)
     {
-        $user = User::where('email', $request->email)->first();
+        $user = Entregador::where('email', $request->email)->first();
 
-        if (! $user || ! Hash::check($request->password, $user->password)) {
+        if (! $user || ! Hash::check($request->password, $user->senha)) {
             return response()->json(['message' => 'Credenciais inválidas'], 401);
         }
 

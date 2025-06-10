704b4fbb71541f90335da58e27301b2bc5a2fc9c
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-06-09 23:03:01 -0300

    Update MobilePedidoController.php

--- app/Http/Controllers/Api/MobilePedidoController.php
+++ app/Http/Controllers/Api/MobilePedidoController.php
@@ -44,7 +44,7 @@ class MobilePedidoController extends Controller
         $motivo = $request->input('motivo');
 
         // Valida status permitido
-        $statusPermitidos = ['aceito', 'recusado', 'cancelado'];
+        $statusPermitidos = ['em andamento', 'recusado', 'cancelado'];
         if (!in_array($status, $statusPermitidos)) {
             return response()->json(['erro' => 'Status inválido'], 400);
         }

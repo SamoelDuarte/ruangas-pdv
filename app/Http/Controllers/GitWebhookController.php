362573c72af6388618644bf3811f27515e856e17
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-06-22 00:39:05 -0300

    Update GitWebhookController.php

--- app/Http/Controllers/GitWebhookController.php
+++ app/Http/Controllers/GitWebhookController.php
@@ -7,21 +7,21 @@ use Illuminate\Http\Request;
 class GitWebhookController extends Controller
 {
    public function handle(Request $request)
-{
-    $output = [];
-    $returnVar = null;
-    $projectPath = base_path();
+   {
+       $output = [];
+       $returnVar = null;
+       $projectPath = base_path();
 
-    $cmd = "cd {$projectPath} && whoami && /usr/bin/git status && /usr/bin/git pull origin main 2>&1";
+       // Comando para resetar e limpar mudanças locais antes do pull
+       $cmd = "cd {$projectPath} && whoami && /usr/bin/git reset --hard && /usr/bin/git clean -fd && /usr/bin/git pull origin main 2>&1";
 
-    exec($cmd, $output, $returnVar);
+       exec($cmd, $output, $returnVar);
 
-    return response()->json([
-        'executed_as' => exec('whoami'),
-        'executed_command' => $cmd,
-        'output' => $output,
-        'return_var' => $returnVar
-    ]);
-}
-
+       return response()->json([
+           'executed_as' => exec('whoami'),
+           'executed_command' => $cmd,
+           'output' => $output,
+           'return_var' => $returnVar
+       ]);
+   }
 }

6dfe633d7a9fa9bbb85e6669c44cd6d7a39a8dbb
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-06-22 15:43:27 -0300

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,49 +9,49 @@ class WebhookController extends Controller
 {
    public function evento(Request $request)
 {
-    $payload = $request->all();
+    // $payload = $request->all();
 
-    // Verifica se veio o evento correto
-    if (!isset($payload['event']) || $payload['event'] !== 'messages.upsert') {
-        return response()->json(['erro' => 'Evento inválido'], 400);
-    }
+    // // Verifica se veio o evento correto
+    // if (!isset($payload['event']) || $payload['event'] !== 'messages.upsert') {
+    //     return response()->json(['erro' => 'Evento inválido'], 400);
+    // }
 
-    $fullJid = $payload['sender'] ?? null;
-    $mensagemTexto = $payload['data']['message']['conversation'] ?? null;
+    // $fullJid = $payload['sender'] ?? null;
+    // $mensagemTexto = $payload['data']['message']['conversation'] ?? null;
 
-    if (!$fullJid || !$mensagemTexto) {
-        return response()->json(['erro' => 'Dados incompletos'], 400);
-    }
+    // if (!$fullJid || !$mensagemTexto) {
+    //     return response()->json(['erro' => 'Dados incompletos'], 400);
+    // }
 
-    // Extrai número (remove @s.whatsapp.net e o 55 do início)
-    $numero = str_replace(['@s.whatsapp.net', '55'], '', $fullJid);
+    // // Extrai número (remove @s.whatsapp.net e o 55 do início)
+    // $numero = str_replace(['@s.whatsapp.net', '55'], '', $fullJid);
 
-    // Busca o pedido com status 8 cujo cliente tem esse telefone
-    $pedido = Pedido::where('status_pedido_id', 8)
-        ->whereHas('cliente', function ($q) use ($numero) {
-            $q->where('telefone', $numero);
-        })
-        ->latest() // caso haja mais de um, pega o mais recente
-        ->first();
-        dd($pedido);
-    if (!$pedido) {
-        return response()->json(['erro' => 'Pedido não encontrado para esse cliente'], 404);
-    }
+    // // Busca o pedido com status 8 cujo cliente tem esse telefone
+    // $pedido = Pedido::where('status_pedido_id', 8)
+    //     ->whereHas('cliente', function ($q) use ($numero) {
+    //         $q->where('telefone', $numero);
+    //     })
+    //     ->latest() // caso haja mais de um, pega o mais recente
+    //     ->first();
+    //     dd($pedido);
+    // if (!$pedido) {
+    //     return response()->json(['erro' => 'Pedido não encontrado para esse cliente'], 404);
+    // }
 
-    // Cria a mensagem recebida
-    $mensagem = \App\Models\Messagen::create([
-        'pedido_id'   => $pedido->id,
-        'messagem'    => $mensagemTexto,
-        'direcao'     => 'recebido',
-        'enviado'     => true, // já foi recebida
-        'device_id'   => null,
-        'usuario_id'  => null,
-    ]);
+    // // Cria a mensagem recebida
+    // $mensagem = \App\Models\Messagen::create([
+    //     'pedido_id'   => $pedido->id,
+    //     'messagem'    => $mensagemTexto,
+    //     'direcao'     => 'recebido',
+    //     'enviado'     => true, // já foi recebida
+    //     'device_id'   => null,
+    //     'usuario_id'  => null,
+    // ]);
 
-    return response()->json([
-        'status' => 'Mensagem registrada',
-        'mensagem' => $mensagem
-    ]);
+    // return response()->json([
+    //     'status' => 'Mensagem registrada',
+    //     'mensagem' => $mensagem
+    // ]);
 }
 
 }

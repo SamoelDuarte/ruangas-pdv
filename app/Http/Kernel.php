00fcfa02c5b97ff62c0b5ccced7e134c30ce25ae
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-06-22 18:33:29 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,13 +22,13 @@ class Kernel extends HttpKernel
     ];
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
-        \App\Http\Middleware\CustomCors::class,  // Adicione aqui
-        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // \App\Http\Middleware\TrustProxies::class,
+        // \Illuminate\Http\Middleware\HandleCors::class,
+        // \App\Http\Middleware\CustomCors::class,  // Adicione aqui
+        // \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        // \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        // \App\Http\Middleware\TrimStrings::class,
+        // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
